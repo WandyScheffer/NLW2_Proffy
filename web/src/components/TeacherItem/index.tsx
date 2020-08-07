23e52f24f507dbe480e11c6b59f415a51ff024b7
Wandy Rosa <wandy.s.scheffer.r@gmail.com>
2020-08-07 08:32:25 -0300

    3rd day of nlw

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,33 +1,60 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher{
+    
+    id: number;
+    subject: string;
+    cost: number;
+    user_id: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.user_id
+        })
+    }
+
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/46692989?s=460&u=270a2463855abf2d8571378acb7fdd732c810c45&v=4" alt="Wandy Rosa" />
+                <img src={teacher.avatar} alt="Wandy Rosa" />
                 <div>
-                    <strong>Wandy Rosa</strong>
-                    <span>Física</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiasta em conhecimentos de física e exterminador de terra planistas.
-                        <br /> <br />
-                        Apaixonado por física e sempre disposto a aprender algo novo sobre as forças da natureza.
-                    </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                            <strong>$ 80,00</strong>
+                    <strong>R$ {teacher.cost},00 </strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="whatsapp" />
                             Entrar em contato
-                        </button>
+                </a>
             </footer>
         </article>
     )
